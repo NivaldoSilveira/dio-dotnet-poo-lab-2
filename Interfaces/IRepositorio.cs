ebd3a692437a524f98b53cb0be79f65725c08baa
Nivaldo <dr.nivaldo@gmail.com>
2021-02-17 11:28:28 -0400

    commit inicial

---
+++ Interfaces/IRepositorio.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace DIO.Series.Interface
+{
+    public interface IRepositorio<T>
+    {
+        List<T> Lista();
+        T RetonaPorId(int id);
+        void Insere(T entidade);
+        void Excluir(int id);
+        void Atualiza(int id, T entidade);
+        int ProximoId();
+         
+    }
+}
